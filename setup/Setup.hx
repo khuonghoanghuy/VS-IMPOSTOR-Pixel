f9f4aa6b457801e9157a7eeb64410b0208de96e4
kenton54 <kentogamerisaneko@gmail.com>
2026-03-04 19:59:46 -0400

    stuff

--- setup/Setup.hx
+++ setup/Setup.hx
@@ -7,6 +7,8 @@ import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
 
+using StringTools;
+
 class Setup
 {
 	public static function main()
@@ -107,6 +109,51 @@ class Setup
 	static function getSystem():String
 	{
 		return Sys.systemName().toLowerCase();
+	}
+}
+
+class Arguments
+{
+	public var length(get, never):Int;
+
+	var arguments:Array<String> = [];
+
+	public function new(args:Array<String>)
+	{
+		for (rawArg in args)
+		{
+			if (rawArg.startsWith('-'))
+			{
+				var argument:String = rawArg.substr(0, 1);
+
+				if (argument.startsWith('-'))
+				{
+					argument = argument.substr(0, 1);
+				}
+
+				arguments.push(argument);
+			}
+		}
+	}
+
+	public function exists(arg:String):Bool
+	{
+		return arguments.indexOf(arg) >= 0;
+	}
+
+	public function getArgument(index:Int):String
+	{
+		return arguments[index];
+	}
+
+	public function getArgumentIndex(argument:String):Int
+	{
+		return arguments.indexOf(argument);
+	}
+
+	function get_length():Int
+	{
+		return arguments.length;
 	}
 }
 
