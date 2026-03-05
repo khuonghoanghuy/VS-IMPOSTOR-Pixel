4263fc00f3042d64ef6ad4586e3e26fd44b079fc
khuonghoanghuy <khuonghoanghuy@gmail.com>
2026-03-05 13:12:38 +0700

    let try this

---
+++ source/funkin/menus/import.hx
@@ -0,0 +1,31 @@
+package funkin.menus;
+
+#if !macro
+#if DISCORD_API
+import funkin.api.DiscordClient;
+#end
+
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+import funkin.Defaults;
+import funkin.Paths;
+import funkin.graphics.FunkinSprite;
+import funkin.graphics.FunkinText;
+import funkin.sound.FunkinSound;
+import funkin.system.Achievements;
+import funkin.system.Conductor;
+import funkin.system.Translations;
+import funkin.ui.MusicBeatState;
+import funkin.utils.Assets;
+import funkin.utils.MathUtil;
+
+using StringTools;
+#end
